c0c404c9b090cdac17b8e52dcac8ccf99916c3ac
LaurenzSoltau <laurenz.soltau@icloud.com>
2023-09-05 20:20:27 +0200

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeValue) {
+    const args = Array.from(arguments);
+    const removeValues = args.slice(1);
+    let returnArray = [];
 
+    for (let value of array) {
+        if (!removeValues.includes(value)) {
+            returnArray.push(value);
+        }
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
