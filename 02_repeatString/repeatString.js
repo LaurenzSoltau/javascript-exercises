ae8d8416c8e32a789f1d0a8e380f6f0a72bcbb92
LaurenzSoltau <laurenz.soltau@icloud.com>
2023-09-05 20:00:28 +0200

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, rounds) {
+    if (rounds < 0) return "ERROR"
+    returnString = "";
+    for (let i = rounds; i > 0; i--) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
